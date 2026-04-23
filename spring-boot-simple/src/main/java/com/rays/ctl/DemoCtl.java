9421036294fd65bbfcbf1766176a47c8b3a63c94
mehreen2103 <mehreenansari2097@gmail.com>
2026-04-23 12:24:26 +0530

    done

---
+++ spring-boot-simple/src/main/java/com/rays/ctl/DemoCtl.java
@@ -0,0 +1,10 @@
+package com.rays.ctl;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="Demo")
+public class DemoCtl {
+
+}
